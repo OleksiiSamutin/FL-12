8be62946f2941cdf25e109eeef2a5f9de13e7569
OleksiiSamutin <samutin63@gmail.com>
2019-12-25 23:38:06 +0200

    Update solving for second task

--- FL12_HW7/homework/src/js/task2.js
+++ FL12_HW7/homework/src/js/task2.js
@@ -1,4 +1,5 @@
-let game = confirm('Do you want to play a game?');
+let game = true;
+let playAgain;
 let max = 8;
 let min = 0;
 let rand;
@@ -6,50 +7,71 @@ let userGuessNumber;
 let attemptsNumber = 3;
 let totalPrize = 0;
 let possiblePrize = 100;
-let minPrize = 25;
-let averPrize = 2 * minPrize;
-let maxPrize = 2 * averPrize;
-if (game) {
-    while (attemptsNumber > 0) {
+
+
+while (game){
+
+    game = confirm('Do you want to play a game?');
+    if (!game){
+        alert('You did not become a billionaire, but can.')
+        break;
+    }
+    while (attemptsNumber>0){
+        let minPrize = 25;
+        let averPrize = 2 * minPrize;
+        let maxPrize = 2 * averPrize;
         let boolContinue;
-        if (attemptsNumber === 1) {
+        if (attemptsNumber === 1){
             possiblePrize = minPrize;
-        } else if (attemptsNumber === 2) {
+        } else if(attemptsNumber === 2){
             possiblePrize = averPrize;
-        } else if (attemptsNumber === 3) {
+        } else if (attemptsNumber === 3){
             possiblePrize = maxPrize;
         }
         rand = Math.floor(Math.random() * (max - min + 1)) + min;
-        userGuessNumber = parseInt(prompt(`Choose a roulete pocket number from ${min} to ${max}
+        userGuessNumber = prompt(`Choose a roulete pocket number from ${min} to ${max}
 Attempts left: ${attemptsNumber}
 Total prize: ${totalPrize}$
-Possible prize on current attempt: ${possiblePrize}$
-rand: ${rand}`, ''))
-        if (attemptsNumber === 3 && userGuessNumber === rand) {
-            totalPrize += maxPrize;
-            boolContinue = confirm(`Congratulation, you won!   Your prize is: ${maxPrize}$. Do you want to continue?`)
+Possible prize on current attempt: ${possiblePrize}$`,'')
+         userGuessNumber = parseInt(userGuessNumber);
+         if (attemptsNumber === 3 && userGuessNumber===rand){
+             totalPrize+=maxPrize;
+             boolContinue=confirm(`Congratulation, you won!   Your prize is: ${maxPrize}$. Do you want to continue?`)
 
-        } else if (attemptsNumber === 2 && userGuessNumber === rand) {
-            totalPrize += averPrize;
-            boolContinue = confirm(`Congratulation, you won!   Your prize is: ${averPrize}$. Do you want to continue?`)
-        } else if (attemptsNumber === 1 && userGuessNumber === rand) {
-            totalPrize += minPrize;
-            boolContinue = confirm(`Congratulation, you won!   Your prize is: ${minPrize}$. Do you want to continue?`)
-        }
-
+         } else if (attemptsNumber === 2 && userGuessNumber===rand){
+            totalPrize+=averPrize;
+            boolContinue=confirm(`Congratulation, you won!   Your prize is: ${averPrize}$. Do you want to continue?`)
+         } else if (attemptsNumber === 1 && userGuessNumber===rand){
+            totalPrize+=minPrize;
+            boolContinue= confirm(`Congratulation, you won!   Your prize is: ${minPrize}$. Do you want to continue?`)
+         }
         attemptsNumber--;
-        if (boolContinue) {
-            maxPrize *= 2;
-            averPrize *= 2;
-            minPrize *= 2;
+        if (boolContinue){
+            maxPrize*= 2;
+            averPrize*= 2;
+            minPrize*= 2;
+            max += 4;
             attemptsNumber = 3;
         }
-        if (attemptsNumber === 0 || !boolContinue && typeof boolContinue !== 'undefined') {
+        if (attemptsNumber === 0 || !boolContinue && typeof boolContinue !== 'undefined'){
             alert(`Thank you for your participation. Your prize is: ${totalPrize}$`)
-            break;
+
+            playAgain = confirm('Do you want to play again?')
+            if (playAgain){
+                attemptsNumber=3;
+                minPrize = 25;
+                max = 8;
+                possiblePrize = 100;
+                totalPrize = 0;
+            }
+            if (!playAgain){
+                game = false;
+                break;
+
+            }
+
+
         }
     }
 
-} else {
-    alert('You did not become a billionaire, but can.')
 }
