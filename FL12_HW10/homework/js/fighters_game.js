3c0ed1554e088148806142187210e703b7f20172
OleksiiSamutin <samutin63@gmail.com>
2020-01-17 15:28:09 +0200

    Finished fighters_game

--- FL12_HW10/homework/js/fighters_game.js
+++ FL12_HW10/homework/js/fighters_game.js
@@ -1,67 +1,107 @@
+const myFighter = new Fighter({ name: 'Maximus', damage: 20, strength: 20, agility: 15, hp: 100 });
+const secondFighter = new Fighter({ name: 'Commodus', damage: 25, strength: 30, agility: 25,hp: 90 });
+
+
+function battle(firstFighter, secondFighter) {
+    if (firstFighter.getHealth() === 0) {
+        return `${firstFighter.getName()} is dead and can't fight.`
+    } else if (secondFighter.getHealth() === 0) {
+        return `${secondFighter.getName()} is dead and can't fight.`
+    } else {
+        while (firstFighter.getHealth() > 0 && secondFighter.getHealth() > 0) {
+            firstFighter.attack(secondFighter);
+            secondFighter.attack(firstFighter);
+        }
+        if (firstFighter.getHealth() === 0) {
+            firstFighter.addLoss();
+            secondFighter.addWin();
+            return `${secondFighter.getName()} has won!`
+        } else {
+            secondFighter.addLoss();
+            firstFighter.addWin();
+            return `${firstFighter.getName()} has won!`
+        }
+    }
+
+}
+
+
 class Fighter {
 
-    constructor({name,damage,hp,strength,agility}){
+    constructor({ name, damage, strength, agility, hp }) {
         this._name = name;
         this._damage = damage;
-        this._hp = hp;
         this._totalHp = hp;
         this._strength = strength;
         this._agility = agility;
+        this._hp = hp;
         this._win = 0;
         this._loss = 0;
     }
 
-    heal(numberOfHeal){
-        if(this._hp + numberOfHeal > this._totalHp){
+    heal(numberOfHeal) {
+        if (this._hp + numberOfHeal > this._totalHp) {
             this._hp = this._totalHp;
-        } else{
-            this._hp+=numberOfHeal;
+        } else {
+            this._hp += numberOfHeal;
         }
 
     }
 
-    addWin(){
+    addWin() {
         this._win++;
     }
 
-    addLoss(){
+    addLoss() {
         this._loss++;
     }
 
-    dealDamage(dealedDamage){
-        if (dealedDamage > this._hp){
+    dealDamage(dealedDamage) {
+        if (dealedDamage > this._hp) {
             this._hp = 0;
-        } else{
+        } else {
             this._hp -= dealedDamage;
         }
 
     }
 
-    logCombatHistory(){
-        return `Name: ${this._name}, Wins: ${this._win}, Losses: ${this._loss}`
+    logCombatHistory() {
+        return `Name: ${this.getName()}, Wins: ${this._win}, Losses: ${this._loss}`
     }
-    getName(){
+
+    attack(defender) {
+        let prob = 100 - defender.getAgility() - defender.getStrength();
+        let real = Number.parseInt(Math.random() * 100);
+        if (prob >= real) {
+            defender.dealDamage(this.getDamage());
+            console.log(`${this._name} makes ${this._damage} damage to ${defender._name}`);
+        } else {
+            console.log(`${this._name} attack missed`);
+        }
+    }
+    getName() {
         return this._name;
     }
 
-    getDamage(){
+    getDamage() {
         return this._damage;
     }
 
-    getStrength(){
+    getStrength() {
         return this._strength;
     }
 
-    getAgility(){
+    getAgility() {
         return this._agility;
     }
 
-    getHealth(){
+    getHealth() {
         return this._hp;
     }
 
 }
-const myFighter = new Fighter({name: 'Maximus', damage: 25, hp: 100, strength: 30, agility: 25});
+const myFighter = new Fighter({ name: 'Maximus', damage: 20, strength: 20, agility: 15, hp: 100 });
+const secondFighter = new Fighter({ name: 'Commodus', damage: 25, strength: 30, agility: 25,hp: 90 });
 let name1 = myFighter.getName();
 console.log(name1);
 let name2 = myFighter._name;
@@ -69,3 +109,10 @@ console.log(myFighter.getDamage());
 console.log(myFighter.getStrength());
 console.log(myFighter.getAgility());
 console.log(myFighter.getHealth());
+battle(myFighter, secondFighter);
+battle(myFighter,secondFighter);
+myFighter.heal(100);
+secondFighter.heal(100);
+battle(secondFighter,myFighter);
+console.log(myFighter.logCombatHistory());
+console.log(secondFighter.logCombatHistory());
